3e79fb85a5d8612e1f60836ef1c2276856f12e04
Hui Ren <huiren16@gmail.com>
2021-07-25 15:04:10 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-data_path = "./data/bank_data.csv"
+data_path = './data/bank_data.csv'
 eda_output_path = './results/eda/eda.html'
 image_output_path = './results/images/'
 model_output_path = './models/rfc_model.pkl'
@@ -8,22 +8,22 @@ cat_columns = [
     'Marital_Status',
     'Income_Category',
     'Card_Category',
-    'Attrition_Flag'             
+    'Attrition_Flag'
 ]
 quant_columns = [
     'Customer_Age',
-    'Dependent_count', 
+    'Dependent_count',
     'Months_on_book',
-    'Total_Relationship_Count', 
+    'Total_Relationship_Count',
     'Months_Inactive_12_mon',
-    'Contacts_Count_12_mon', 
-    'Credit_Limit', 
+    'Contacts_Count_12_mon',
+    'Credit_Limit',
     'Total_Revolving_Bal',
-    'Avg_Open_To_Buy', 
-    'Total_Amt_Chng_Q4_Q1', 
+    'Avg_Open_To_Buy',
+    'Total_Amt_Chng_Q4_Q1',
     'Total_Trans_Amt',
-    'Total_Trans_Ct', 
-    'Total_Ct_Chng_Q4_Q1', 
+    'Total_Trans_Ct',
+    'Total_Ct_Chng_Q4_Q1',
     'Avg_Utilization_Ratio'
 ]
 target = 'Attrition_Flag_Existing Customer'
